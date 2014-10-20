11fb7d6ca1ac8692db66ebf684be1bb84136fa82
Ivaylo Georgiev <georgievir@yahoo.com>
2014-10-20 16:22:29 +0100

    Added base WebForms project. Edited .gitignore to actually ignore the folders in /packages/* and allow only the packages.config file.

---
+++ AccountSystem/AccountSystem.WebForms/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AccountSystem.WebForms.Global" Language="C#" %>
---
+++ AccountSystem/AccountSystem.WebForms/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AccountSystem.WebForms.Startup))]
+namespace AccountSystem.WebForms
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
