284f67e9112ff79458e92425872aeb0bc3af80e0
Ivaylo Georgiev <georgievir@yahoo.com>
2014-10-23 14:16:27 +0100

    Added initial user seed.

--- AccountSystem/AccountSystem.Data/Migrations/Configuration.cs
+++ AccountSystem/AccountSystem.Data/Migrations/Configuration.cs
@@ -45,6 +45,43 @@ namespace AccountSystem.Data.Migrations
                 manager.Create(user, "admin1");
                 manager.AddToRole(user.Id, "Admin");
             }
+
+            if (!context.Users.Any(u => u.UserName == "pesho"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { UserName = "pesho", Email = "pesho@pesho.com" };
+
+                manager.Create(user, "pesho1");
+                manager.AddToRole(user.Id, "Client");
+
+                var acc = new BankAccount() { Balance = 521.25m, CurrencyType = CurrencyType.BGN, OwnerId = user.Id };
+                context.Accounts.Add(acc);
+                context.SaveChanges();
+
+                var card = new Card() { CardNumber = "1234567890123456", CardStatus = CardStatus.Approved, CardType = CardType.MasterCard, ExpirationDate = new DateTime(2018, 5, 5), Pin = "0000", AccountId = acc.Id, OwnerId = user.Id };
+                context.Cards.Add(card);
+                context.SaveChanges();
+
+            }
+
+            if (!context.Users.Any(u => u.UserName == "gosho"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { UserName = "gosho", Email = "gosho@gosho.com" };
+
+                manager.Create(user, "gosho1");
+                manager.AddToRole(user.Id, "Client");
+
+                var acc = new BankAccount() { Balance = 100000m, CurrencyType = CurrencyType.BGN, OwnerId = user.Id };
+                context.Accounts.Add(acc);
+                context.SaveChanges();
+
+                var card = new Card() { CardNumber = "9876543210012345", CardStatus = CardStatus.Approved, CardType = CardType.Visa, ExpirationDate = new DateTime(2019, 5, 5), Pin = "0000", AccountId = acc.Id, OwnerId = user.Id };
+                context.Cards.Add(card);
+                context.SaveChanges();
+            }
         }
     }
 }
