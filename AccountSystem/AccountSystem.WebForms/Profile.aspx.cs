abc72d75644b4f659f13d2370a73dad19f20707e
Ivaylo Georgiev <georgievir@yahoo.com>
2014-10-23 10:18:09 +0100

    For some reason the cs file for profile didnt get added on last commit

---
+++ AccountSystem/AccountSystem.WebForms/Profile.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using AccountSystem.Data;
+using AccountSystem.Models;
+
+namespace AccountSystem.WebForms
+{
+    public partial class Profile : System.Web.UI.Page
+    {
+        List<BankAccount> accounts;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                Response.Redirect("~/Account/Login");
+            }
+            
+            var userId = User.Identity.GetUserId();
+
+            var data = new AccountSystemData(new AccountSystemDbContext());
+
+            accounts = data.Accounts.All().Where(x => x.OwnerId == userId).ToList();
+
+            AccountsRepeater.DataSource = accounts;
+            AccountsRepeater.DataBind();
+        }
+    }
+}
